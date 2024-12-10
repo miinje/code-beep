b9b7df90eb3e39903d85f752b4ebdb2994ed9c78
miinje <njjj00083@gmail.com>
2024-12-10 11:40:09 +0900

    Design: Login(앱 진입 시 첫 화면) UI 작업 완료
    - UI 작업에 필요한 png 파일 적용
    + app.json: splash backgroud 색상 변경

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
+import CustomButton from "./components/Custombutton";
+import CustomText from "./components/CustomText";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CustomText
+        text="Code Beep에 오신 것을 환영합니다!"
+        style={{
+          fontSize: 20,
+          margin: 20,
+          color: "#fff",
+        }}
+      />
+      <ImageBackground
+        source={require("./assets/code-beep-bubbles.png")}
+        style={styles.bubbles}
+      >
+        <View style={styles.bubblesBox}>
+          <CustomText text="구글 로그인은" />
+          <CustomText text="깃헙이 가입된" />
+          <CustomText text="구글 계정만" />
+          <CustomText text="가능해요! >_" />
+        </View>
+      </ImageBackground>
+      <Image
+        source={require("./assets/code-beep-icon.png")}
+        style={styles.icon}
+      />
+      <CustomButton
+        title="Github Login"
+        source={require("./assets/github-logo.png")}
+      />
+      <View style={{ width: 250, height: 55 }}>
+        <Image
+          style={{ width: "100%", height: "100%" }}
+          source={require("./assets/android_light_rd.png")}
+        />
+      </View>
+      <CustomText
+        text="로그인 후 이용하실 수 있습니다"
+        style={{
+          fontSize: 14,
+          margin: 10,
+          color: "#808080",
+        }}
+      />
     </View>
   );
 }
@@ -16,5 +56,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#404040",
+    gap: 15,
+  },
+  icon: {
+    resizeMode: "center",
+    width: 150,
+    height: 150,
+    marginBottom: 5,
+  },
+  bubbles: {
+    flex: 0,
+    width: 150,
+    height: 150,
+  },
+  bubblesBox: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    marginBottom: 20,
   },
 });
