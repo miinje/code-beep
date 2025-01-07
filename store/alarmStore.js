57ac8f4801d2133275dabbbb9424c131cb78c981
정민지 <chungminjee@jeongminjis-MacBook-Pro.local>
2025-01-07 16:36:01 +0900

    Feat(alarmStore): 전역 객체 alarmQuiz 추가 - 퀴즈에 쓰일 코드 정보 저장

--- store/alarmStore.js
+++ store/alarmStore.js
@@ -4,9 +4,11 @@ const alarmStore = create((set) => ({
   allAlarmData: null,
   isTimeMatched: false,
   currentTime: new Date(),
+  alarmQuiz: null,
   setAllAlarmData: (state) => set({ allAlarmData: state }),
   setIsTimeMatched: (state) => set({ isTimeMatched: state }),
   setCurrentTime: (state) => set({ currentTime: state }),
+  setAlarmQuiz: (state) => set({ alarmQuiz: state }),
 }));
 
 export default alarmStore;
