0511e0efaa15ceea668691147d392a48047471f5
정민지 <chungminjee@jeongminjis-MacBook-Pro.local>
2025-01-09 05:18:47 +0900

    Feat(ActionAlarm): 퀴즈 제공, 맞힐 시 AlarmList로 이동

--- app/ActionAlarm.js
+++ app/ActionAlarm.js
@@ -1,103 +1,248 @@
+import { useFonts } from "expo-font";
 import { useRouter } from "expo-router";
-import { useEffect } from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import CustomText from "../components/CustomText";
 import alarmStore from "../store/alarmStore";
 import { stopAudio } from "../utils/audioPlayer";
 
 export default function ActionAlarm() {
-  const { currentTime, setIsTimeMatched, setCurrentTime } = alarmStore();
+  const { currentTime, alarmQuiz, setCurrentTime, setIsTimeMatched } =
+    alarmStore();
+  const [inputValue, setInputValue] = useState("");
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
   const router = useRouter();
-
-  const handleClickDone = async () => {
-    setIsTimeMatched(false);
-
-    await stopAudio();
-
-    router.replace("/AlarmList");
-  };
+  const [isFontsLoaded] = useFonts({
+    CONSOLA: require("../assets/fonts/CONSOLA.ttf"),
+  });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentTime(new Date());
+      const now = new Date();
+
+      setCurrentTime(now);
     }, 1000);
 
     return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", (e) => {
+      setKeyboardVisible(true);
+    });
+
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardVisible(false);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  const handleClickDone = async () => {
+    Keyboard.dismiss();
+
+    if (inputValue !== "Hi") {
+      Alert.alert("오답입니다!", "다시 생각해 보세요", [
+        { text: "OK", onPress: () => setInputValue("") },
+      ]);
+    } else {
+      setIsTimeMatched(false);
+
+      await stopAudio();
+
+      router.replace("/AlarmList");
+    }
+  };
+
+  const alarmQuizItems = alarmQuiz[0].functionBody.map((text) => {
+    const returnValue = alarmQuiz[0].returnValues[0];
+    let returnBlankSpace = "";
+
+    if (text.includes(returnValue)) {
+      text = text.slice(0, text.length - returnValue.length - 1);
+      returnBlankSpace = "".padEnd(returnValue.length, " ");
+    }
+
+    return (
+      <View
+        key={text}
+        style={{ flex: 0, flexDirection: "row", width: "100%", gap: 10 }}
+      >
+        <Text style={styles.quizText}>{text}</Text>
+        {returnBlankSpace.length !== 0 ? (
+          <Text
+            style={{
+              backgroundColor: "#E0E0E0",
+              borderRadius: 5,
+              flex: 0,
+              flexDirection: "row",
+            }}
+          >
+            {returnBlankSpace}
+          </Text>
+        ) : null}
+      </View>
+    );
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topBox}>
-        <View style={styles.currentTimeBox}>
-          <CustomText
-            text={
-              String(currentTime.getHours()).length < 2
-                ? "0" + currentTime.getHours()
-                : currentTime.getHours()
-            }
-            size={80}
-          />
-          <CustomText text=":" size={80} />
-          <CustomText
-            text={
-              String(currentTime.getMinutes()).length < 2
-                ? "0" + currentTime.getMinutes()
-                : currentTime.getMinutes()
-            }
-            size={80}
-          />
-          <CustomText text=":" size={80} />
-          <CustomText
-            text={
-              String(currentTime.getSeconds()).length < 2
-                ? "0" + currentTime.getSeconds()
-                : currentTime.getSeconds()
-            }
-            size={80}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.topBox}>
+          <View style={styles.currentTimeBox}>
+            <CustomText
+              text={
+                String(currentTime.getHours()).length < 2
+                  ? "0" + currentTime.getHours()
+                  : currentTime.getHours()
+              }
+              size={80}
+            />
+            <CustomText text=":" size={80} />
+            <CustomText
+              text={
+                String(currentTime.getMinutes()).length < 2
+                  ? "0" + currentTime.getMinutes()
+                  : currentTime.getMinutes()
+              }
+              size={80}
+            />
+            <CustomText text=":" size={80} />
+            <CustomText
+              text={
+                String(currentTime.getSeconds()).length < 2
+                  ? "0" + currentTime.getSeconds()
+                  : currentTime.getSeconds()
+              }
+              size={80}
+            />
+          </View>
+          <CustomText text="일어날 시간이에요!" size={15} />
+        </View>
+        <View
+          style={[
+            styles.quizTextContainer,
+            { marginBottom: isKeyboardVisible ? 150 : 0 },
+          ]}
+        >
+          <View style={styles.quizTextBox}>
+            <View style={styles.fileRootText}>
+              <Text style={styles.rootText}>{alarmQuiz[0].filesRoot}</Text>
+              <Text style={styles.rootText}>JavaScript</Text>
+            </View>
+            <View style={{ flex: 0, gap: 5, marginTop: 10 }}>
+              {alarmQuizItems}
+            </View>
+          </View>
+          <TextInput
+            style={styles.textInput}
+            value={inputValue}
+            onChangeText={setInputValue}
+            placeholder="빈칸에 옳은 답을 쓰시오"
+            placeholderTextColor="#000"
           />
         </View>
-        <CustomText text="일어날 시간이에요!" size={15} />
+        {!isKeyboardVisible ? (
+          <TouchableOpacity
+            style={[
+              styles.button,
+              {
+                backgroundColor: !inputValue ? "#C0C0C0" : "#ffffff",
+              },
+            ]}
+            onPress={handleClickDone}
+            disabled={!inputValue}
+          >
+            <View style={styles.buttonInnerBox}>
+              <CustomText
+                text="완료!"
+                textColor={!inputValue ? "#242424" : "#000000"}
+                size={21}
+              />
+            </View>
+          </TouchableOpacity>
+        ) : null}
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleClickDone}>
-        <View style={styles.buttonInnerBox}>
-          <CustomText text="완료!" textColor="#000000" size={21} />
-        </View>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#404040",
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  topBox: {
+    flex: 0,
+    width: 400,
+    height: 150,
+    gap: 10,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#404040",
-    gap: 10,
-  },
-  titleText: {
-    fontSize: 80,
   },
   currentTimeBox: {
     flex: 0,
+    marginTop: 40,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     gap: 5,
   },
-  topBox: {
-    flex: 3,
-    gap: 5,
-    alignItems: "center",
+  quizTextContainer: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    width: 370,
+    height: 100,
+    gap: 20,
   },
-  button: {
+  fileRootText: {
     flex: 0,
-    width: "85%",
-    marginBottom: "10%",
-    height: 55,
-    borderRadius: 50,
+    width: 350,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: -10,
+  },
+  quizTextBox: {
+    width: 370,
+    height: 350,
+    backgroundColor: "#383838",
+    padding: 20,
+    borderRadius: 15,
+  },
+  rootText: {
+    fontFamily: "CONSOLA",
+    color: "#C0C0C0",
+    fontSize: 9,
+  },
+  quizText: {
+    fontFamily: "CONSOLA",
+    color: "#C0C0C0",
+    fontSize: 12,
+  },
+  textInput: {
     backgroundColor: "#ffffff",
+    fontFamily: "CONSOLA",
+    textAlign: "center",
+    width: 350,
+    height: 50,
+    borderRadius: 10,
   },
   buttonInnerBox: {
     flex: 1,
@@ -107,5 +252,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     textAlign: "center",
+  },
+  button: {
+    width: "85%",
+    height: 55,
+    borderRadius: 50,
+    backgroundColor: "#ffffff",
   },
 });
